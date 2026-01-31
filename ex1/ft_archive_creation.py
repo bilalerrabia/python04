02eedb34d82ad7d185feed2a1cee6716b7eb49a2
bilal errabia <115508904+bilalerrabia@users.noreply.github.com>
2026-01-31 20:30:39 +0100

    Fix indentation and clean up print statements

--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -13,7 +13,6 @@ new_file.write("[ENTRY 003] Archived by Data Archivist trainee\n")
 print("[ENTRY 001] New quantum algorithm discovered")
 print("[ENTRY 002] Efficiency increased by 347%")
 print("[ENTRY 003] Archived by Data Archivist trainee")
-
 new_file.close()
 print("\nData inscription complete. Storage unit sealed.")
 print(f"Archive '{filename}' ready for long-term preservation.")
